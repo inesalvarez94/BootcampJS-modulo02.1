488b791ba532138f10099d84f29d50f121328cea
Maria Ines Alvarez Sanmartin <maria.ines.alvarez.1994@gmail.com>
2025-04-23 10:00:45 +0200

    01

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+function calcularPrecioPorPersona (precioTotal, descuento, numeroDeAmigos){
+    return (precioTotal - descuento) / numeroDeAmigos
+}
+
+const precioTotal = 120;
+const descuento = 18;
+const numeroDeAmigos = 6;
+const precioPorPersona = calcularPrecioPorPersona (precioTotal, descuento, numeroDeAmigos)
+
+console.log(`El precio por persona es: ${precioPorPersona}`);
+console.log(`El precio que pago yo es: ${precioPorPersona + descuento}`);
